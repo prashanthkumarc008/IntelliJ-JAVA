4f2aefc7f53260bfe5a984bd1666289fd29894c1
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-07-09 12:22:37 +0530

    sg

--- untitled8/src/java8/FunctionINTF.java
+++ untitled8/src/java8/FunctionINTF.java
@@ -2,8 +2,18 @@ package java8;
 
 import com.sun.tools.javac.Main;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class FunctionINTF {
     public static void main(String[] args) {
+        List<Integer> numbers= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Function<List,Integer> f=nums-> nums.size();
+        System.out.println(f.apply(numbers));
+        Predicate<List> p=nums-> nums.isEmpty();
+        System.out.println(p.test(numbers));
 
     }
 }
